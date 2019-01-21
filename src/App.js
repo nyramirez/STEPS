b3fe94148ba23a9d9ecd0ea3934bbc3972567c83
Thanh Dang <Thanhqd1990@gmail.com>
2019-01-20 23:08:20 -0600

    Initial commit for Thanh branch: Placeholder route component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Nav from "./components/Nav/Nav";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="container">
-    
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/page1" component={Page1} />
+          <Route exact path="/page2" component={Page2} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = props => (
+  <div>
+          <a href="/page1">
+            Page 1
+          </a>
+          <span> | </span>
+          <a href="/page2">
+             Page 2
+          </a>
+  </div>
+);
+
+export default Nav;
