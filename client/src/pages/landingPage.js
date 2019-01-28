673c55b97ae0262913e4e9f6699eaa75141779d5
Akanksha Kapoor <agaur05@gmail.com>
2019-01-27 23:51:35 -0600

    adding footer

--- client/src/pages/landingPage.js
+++ client/src/pages/landingPage.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import NavLandingPage from "../components/LandingNav/NavLandingPage";
 import LandingBody from "../components/LandingBody/LandingBody"
-
+import Footer from "../components/Footer/Footer"
 class LandingPage extends Component {
 
     render() {
       return (<div>
     <NavLandingPage/>
     <LandingBody/>
+    <Footer/>
       </div>
       );
     }
