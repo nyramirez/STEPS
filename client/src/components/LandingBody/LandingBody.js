a554b748e9005365e12cd01dcdf72c5845fe5bde
Akanksha Kapoor <agaur05@gmail.com>
2019-01-29 01:05:15 -0600

    final changes

--- client/src/components/LandingBody/LandingBody.js
+++ client/src/components/LandingBody/LandingBody.js
@@ -10,141 +10,161 @@ import './style.css';
 function LandingBody() {
     return (
         <section className="pt-5 mb">
-        <div className="container">
-            <div className="row">
-                <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 mb-5">
-                
-                    <div id="carousel-5" className="carousel slide" data-ride="carousel" data-interval="4000">
-                       
-                        <ol className="carousel-indicators">
-                          <li data-target="#carousel-5" data-slide-to="0" className="active"></li>
-                          <li data-target="#carousel-5" data-slide-to="1"></li>
-                          <li data-target="#carousel-5" data-slide-to="2"></li>
-                        </ol>
-                       
-                        <div className="carousel-inner" role="listbox">
-                
-                          <div className="carousel-item imageSpace active">
-                            <img className="card-img" src={KidswComputer} alt="slide"/>
-                          </div>
-                          
-                          <div className="carousel-item imageSpace">
-                            <img className="card-img" src={CoffeStudy} alt="slide"/>
-                          </div>
-                          
-                          <div className="carousel-item imageSpace">
-                            <img className="card-img" src={KidSwim} alt="slide"/>
-                          </div>
-                          <div className="carousel-item imageSpace">
-                            <img className="card-img" src={People} alt="slide"/>
-                          </div>
-    
-                        </div>
-                        
-                        <a className="carousel-control-prev" href="#carousel-5" role="button" data-slide="prev">
-                          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span className="sr-only">prev</span>
-                        </a>
-                        
-                        <a className="carousel-control-next" href="#carousel-5" role="button" data-slide="next">
-                          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span className="sr-only">next</span>
-                        </a>
-                        
-                    </div>
-                
-                </div>
-                
-                <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 mb-5">
-                
-                    <div id="carousel-6" className="carousel slide" data-ride="carousel" data-interval="4000">
-                       
-                        <ol className="carousel-indicators">
-                          <li data-target="#carousel-6" data-slide-to="0" className="active"></li>
-                          <li data-target="#carousel-6" data-slide-to="1"></li>
-                          <li data-target="#carousel-6" data-slide-to="2"></li>
-                        </ol>
-                       
-                        <div className="carousel-inner" role="listbox">
-                          <div className="carousel-item active">
-                          <div className="card bg-light mb-3 textSpace" >
-                                    <div className="card-header"><h1 className="cardHeading">~~Skills~~</h1></div>
-                                    <div className="card-body">
-                                        <h3 className="card-title">Everyone has a skill</h3>
-                                        <p className="card-text">It does not matter who you are or what you do.</p>
-                                        <br/>
-                                        <h2 className="card-text">YOU HAVE SKILLS.</h2>
-                                        <br/>
-                                        <p className="card-text">We would like you to teach us.</p>
-                                    </div>
-                                </div>
-                          </div>
-                          
-                          <div className="carousel-item">
-                          <div className="card bg-light mb-3 textSpace" >
-                                    <div className="card-header"><h1 className="cardHeading">~~Teach~~</h1></div>
-                                    <div className="card-body">
-                                        <h3 className="card-title">Tell us about your expertise</h3>
-                                        <br/>
-                                        <p className="card-text">STEPS is an app for you to promote your skill set and teach it to others</p>
-                                    </div>
-                                </div>
-                          </div>
-                          
-                          <div className="carousel-item">
-                          <div className="card bg-light mb-3 textSpace" >
-                                    <div className="card-header"><h1 className="cardHeading">~~Learn~~</h1></div>
-                                    <div className="card-body">
-                                        <h3 className="card-title">Are you looking for a professor of a subject and you cannot find it ?</h3>
-                                        <br/>
-                                        <p className="card-text">Look for you professor here. Math, history, Java, swiming lessons,  whatever you imagine, there is a person willing to teach it.</p>
-                                    </div>
-                                </div>
-                          </div>
-                          < div className="carousel-item">
-                          <div className="card bg-light mb-3 textSpace" >
-                                    <div className="card-header"><h1 className="cardHeading">~~Quality Learning~~</h1></div>
-                                    <div className="card-body">
-                                        <h3 className="card-title">Background check</h3>
-                                        <p className="card-text">All professors and student have to pass a background check, we want to keep you safe.</p>
-                                        <br/>
-                                        <h3 className="card-title">Your decision</h3>
-                                        <p className="card-text">Choose based on rating, qualifications and certification who will teach your next className.</p>
-                                        <br/>
-                                        <h3 className="card-title">Your money</h3>
-                                        <p className="card-text">You as a professor, choose your hourly rate, cost per className and the lenght of the className.</p>
-                                        <br/>
-                                        <h3 className="card-title">Your time</h3>
-                                        <p className="card-text">You choose the className time and the location.</p>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 mb-5">
 
+                        <div id="carousel-5" className="carousel slide" data-ride="carousel" data-interval="4000">
+
+                            <ol className="carousel-indicators">
+                                <li data-target="#carousel-5" data-slide-to="0" className="active"></li>
+                                <li data-target="#carousel-5" data-slide-to="1"></li>
+                                <li data-target="#carousel-5" data-slide-to="2"></li>
+                            </ol>
+
+                            <div className="carousel-inner" role="listbox">
+
+                                <div className="carousel-item imageSpace active">
+                                    <img className="card-img" src={KidswComputer} alt="slide" />
+                                </div>
+
+                                <div className="carousel-item imageSpace">
+                                    <img className="card-img" src={CoffeStudy} alt="slide" />
+                                </div>
+
+                                <div className="carousel-item imageSpace">
+                                    <img className="card-img" src={KidSwim} alt="slide" />
+                                </div>
+                                <div className="carousel-item imageSpace">
+                                    <img className="card-img" src={People} alt="slide" />
+                                </div>
+
+                            </div>
+
+                            <a className="carousel-control-prev" href="#carousel-5" role="button" data-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="sr-only">prev</span>
+                            </a>
+
+                            <a className="carousel-control-next" href="#carousel-5" role="button" data-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="sr-only">next</span>
+                            </a>
+
+                        </div>
+
+                    </div>
+
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 mb-5">
+
+                        <div id="carousel-6" className="carousel slide" data-ride="carousel" data-interval="4000">
+
+                            <ol className="carousel-indicators">
+                                <li data-target="#carousel-6" data-slide-to="0" className="active"></li>
+                                <li data-target="#carousel-6" data-slide-to="1"></li>
+                                <li data-target="#carousel-6" data-slide-to="2"></li>
+                            </ol>
+
+                            <div className="carousel-inner" role="listbox">
+                                <div className="carousel-item active">
+                                    <div className="card" >
+                                        <div className="card-header"><h1 className="cardHeading">~~Skills~~</h1></div>
+                                        <div className="card-body">
+                                            <h3 className="card-title">Everyone has a skill</h3>
+                                            <br />
+                                            <ul>
+                                                <li><p className="card-text">It does not matter who you are or what you do.</p>
+                                                </li>
+                                                <br />
+                                                <li><h2 className="card-text">If you have skills,we would like you to teach us.</h2>
+                                                </li> <br />
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
-                          </div>
-    
+
+                                <div className="carousel-item">
+                                    <div className="card" >
+                                        <div className="card-header"><h1 className="cardHeading">~~Teach~~</h1></div>
+                                        <div className="card-body">
+                                            <h3 className="card-title">Tell us about your expertise</h3>
+                                            <br />
+                                            <ul>
+                                                <p className="card-text">We promote your:</p>
+                                                <li>
+                                                    <p className="card-text">Skill Set &</p>
+                                                </li>
+                                                <li><p className="card-text">Talent</p></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="carousel-item">
+                                    <div className="card" >
+                                        <div className="card-header"><h1 className="cardHeading">~~Learn~~</h1></div>
+                                        <div className="card-body">
+                                            <h3 className="card-title">Looking for a right professor for a subject?</h3>
+                                            <br />
+                                            <ul>
+                                                <p className="card-text">We can arrange for all:</p>
+                                                <li><p className="card-text"> School Subjects, </p></li>
+                                                <li><p className="card-text"> Computer SCience, </p></li>
+                                                <li><p className="card-text"> Swimming, </p></li>
+                                                <li><p className="card-text"> Music, </p></li>
+                                                <li><p className="card-text"> Dancing, etc. etc. </p></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                < div className="carousel-item">
+                                    <div className="card" >
+                                        <div className="card-header"><h1 className="cardHeading">~~Quality Learning~~</h1></div>
+                                        <div className="card-body">
+                                        <h3 className="card-title">We care ,that's why:</h3>
+
+                                            <ul>
+                                                {/* <h3 className="card-text">For safety,must background check for students and professors </h3> */}
+                                                <li>  <p className="card-text">For safety,must background check for students and professors.</p>
+                                                    {/* <h3 className="card-title">Your decision</h3> */}
+                                                </li>
+                                                <li> <p className="card-text">Students can choose from high rated professors.</p>
+                                                    {/* <h3 className="card-title">Your money</h3> */}
+                                                </li>
+                                                <li>  <p className="card-text">Professors can set their hourly rates.</p>
+                                                </li>
+                                                {/* <h3 className="card-title">Your time</h3> */}
+                                                <li> <p className="card-text">Easy to choose class time and location</p>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <a className="carousel-control-prev" href="#carousel-6" role="button" data-slide="prev">
+                                {/* <span className="carousel-control-prev-icon" aria-hidden="true"></span> */}
+                                <span className="sr-only">prev</span>
+                            </a>
+
+                            <a className="carousel-control-next" href="#carousel-6" role="button" data-slide="next">
+                                {/* <span className="carousel-control-next-icon" aria-hidden="true"></span> */}
+                                <span className="sr-only">next</span>
+                            </a>
+
                         </div>
-                        
-                        <a className="carousel-control-prev" href="#carousel-6" role="button" data-slide="prev">
-                          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span className="sr-only">prev</span>
-                        </a>
-                        
-                        <a className="carousel-control-next" href="#carousel-6" role="button" data-slide="next">
-                          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span className="sr-only">next</span>
-                        </a>
-                        
+
                     </div>
-                
+
                 </div>
-                
             </div>
-        </div>
-    </section>
-            );
-        }
-        
+        </section>
+    );
+}
+
 LandingBody.propTypes = {
-                classNamees: PropTypes.object.isRequired,
-        };
-        
+    classNamees: PropTypes.object.isRequired,
+};
+
 export default LandingBody;
