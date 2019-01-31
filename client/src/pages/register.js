19202061310ee933b00998bb23363295ef1627ac
Nefas <nyramirez@gmail.com>
2019-01-30 18:07:58 -0600

    register page step 1, no profile

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import RegNav from "../components/RegistrationNav/regNav";
+import RegBody from "../components/RegistrationBody/RegBody";
+
 
 class Register extends Component {
 
-  render() {
-    return (
-      <h1>Register</h1>
-    );
+    render() {
+      return (<div>
+    <RegNav/>
+    <RegBody/>
+      </div>
+      );
+    }
   }
-}
-
-export default Register;
+  
+  export default Register;
