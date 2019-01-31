0b47d28caf45223a1dfeb1cfd1b11451e11c2a2e
Thanh Dang <Thanhqd1990@gmail.com>
2019-01-30 19:20:14 -0600

    Register model and route work in postman

--- models/Profile.js
+++ models/Profile.js
@@ -26,65 +26,6 @@ const ProfileSchema = new Schema({
   bio: {
     type: String
   },
-  experience: [
-    {
-      title: {
-        type: String,
-        required: true
-      },
-      company: {
-        type: String,
-        required: true
-      },
-      location: {
-        type: String
-      },
-      from: {
-        type: Date,
-        required: true
-      },
-      to: {
-        type: Date
-      },
-      current: {
-        type: Boolean,
-        default: false
-      },
-      description: {
-        type: String
-      }
-    }
-  ],
-  education: [
-    {
-      school: {
-        type: String,
-        required: true
-      },
-      degree: {
-        type: String,
-        required: true
-      },
-      fieldofstudy: {
-        type: String,
-        required: true
-      },
-      from: {
-        type: Date,
-        required: true
-      },
-      to: {
-        type: Date
-      },
-      current: {
-        type: Boolean,
-        default: false
-      },
-      description: {
-        type: String
-      }
-    }
-  ],
   date: {
     type: Date,
     default: Date.now
