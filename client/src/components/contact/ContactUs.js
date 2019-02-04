10aa54fb3fa8bf978b47fffab417575d99e4a52d
Akanksha Kapoor <agaur05@gmail.com>
2019-02-03 22:42:54 -0600

    Updating contact page

--- client/src/components/contact/ContactUs.js
+++ client/src/components/contact/ContactUs.js
@@ -1,68 +1,47 @@
 import React, { Component } from "react";
 import "./font-awesome-4.7.0/css/font-awesome.min.css";
 import contactUsImage from "../../images/contactUs.jpg";
+import 'jquery';
 import "./style.css";
+
 
 class ContactUs extends Component {
     render() {
-      return (
-        <div className="container-fluid mt-3">
-        <div className="row">
-          <div className="col-md-7">
-            <img className="contactImg" src={contactUsImage} alt="slide" />
-          </div>
-          <div className="col-md-5">
-          <section className="contact">
-                    <div className="container bottomWrapper">
-                        <h3 className="text-center text-uppercase text-secondary mb-0">Contact Us</h3>
-                        <hr className="star-dark mb-5" />
-                        <div className="row">
-                            <div className="col-lg-8 mx-auto">
-                                <form className="contactForm">
-                                    <div className="control-group">
-                                        <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                                            <label>Name</label>
-                                            <input className="form-control name"  type="text" placeholder="Name" required="required" data-validation-required-message="Please enter your name." />
-                                            <p className="help-block text-danger"></p>
-                                        </div>
-                                    </div>
-                                    <div className="control-group">
-                                        <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                                            <label>Email</label>
-                                            <input className="form-control email" type="email" placeholder="Email" required="required" data-validation-required-message="Please enter your email address." />
-                                            <p className="help-block text-danger"></p>
-                                        </div>
-                                    </div>
-                                    {/* <div className="control-group">
-                                        <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                                            <label>Phone Number</label>
-                                            <input className="form-control phone" type="tel" placeholder="Phone Number" required="required" data-validation-required-message="Please enter your phone number." />
-                                            <p className="help-block text-danger"></p>
-                                        </div>
-                                    </div> */}
-                                    <div className="control-group">
-                                        <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                                            <label>Message</label>
-                                            <textarea className="form-control message" rows="3" placeholder="Message" required="required" data-validation-required-message="Please enter a message."></textarea>
-                                            <p className="help-block text-danger"></p>
-                                        </div>
-                                    </div>
-                                    <br />
-                                    <div className="success"></div>
-                                    <div className="form-group">
-                                        <button type="submit" className="btn btn-primary btn-xl sendMessageButton">Send</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
+        return (
+            <div className="container-fluid mt-3">
+                <div className="row">
+                    <div className="col-md-7">
+                        <img className="contactImg" src={contactUsImage} alt="slide" />
                     </div>
-                </section>
-          </div>
-        </div>
-      </div>
-      );
+                    <div className="col-md-5">
+                        <section className="contact">
+                            <div className="container bottomWrapper">
+                                <h3 className="text-center text-uppercase text-secondary mb-0">Contact Us</h3>
+                                <hr className="star-dark mb-5" />
+                                <div className="row">
+                                    <div className="col-lg-8 mx-auto">
+                                        <div className="control-group">
+                                            <div className="form-group floating-label-form-group controls mb-0 pb-2">
+                                                <p className="mainLabel">Ask a <strong>STEPS</strong> expert how we can help:</p>
+                                                <p className="contactLabel">Steps - Houston</p>
+                                                   <p className="contactLabel" > 
+                                                   Address: 3030 My Address, Suite 300
+                                                            Houston, Texas 77042</p>
+                                                   <p className="contactLabel" >     
+                                                   Contact : XXX-XXX-XXXX
+                                                    </p>
+                                                <p className="contactLabel">Email: stepshelpteam@gmail.com</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        );
     }
-  }
-  
-  export default ContactUs;
-  
+}
+
+export default ContactUs;
